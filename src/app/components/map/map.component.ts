a821a87a4054adb458208406a3870455ce7de30f
Daniel Warren <daniel@Catrinas-MacBook-Air.local>
2025-10-15 08:14:19 +1300

    feat: use new esri guidelines for adding map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,11 +1,12 @@
 /* eslint-disable object-curly-newline */
-import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import Basemap from '@arcgis/core/Basemap';
 import WMTSLayer from '@arcgis/core/layers/WMTSLayer';
-import Map from '@arcgis/core/Map';
 import MapView from '@arcgis/core/views/MapView';
-import Compass from '@arcgis/core/widgets/Compass';
-import ScaleBar from '@arcgis/core/widgets/ScaleBar';
-import Search from '@arcgis/core/widgets/Search';
+import '@arcgis/map-components/dist/components/arcgis-compass';
+import '@arcgis/map-components/dist/components/arcgis-map';
+import '@arcgis/map-components/dist/components/arcgis-scale-bar';
+import '@arcgis/map-components/dist/components/arcgis-zoom';
 import { environment } from 'secrets';
 
 @Component({
@@ -13,15 +14,16 @@ import { environment } from 'secrets';
   imports: [],
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+
 })
 export class MapComponent implements OnInit, OnDestroy {
-  @ViewChild('view', { static: true }) mapViewDiv: ElementRef | undefined;
+  @ViewChild('view', { static: true }) mapViewDiv!: ElementRef;
 
   private view!: MapView;
 
   ngOnInit(): void {
     this.initializeMap();
-    this.initialiazeWidgets();
   }
 
   ngOnDestroy(): void {
@@ -45,45 +47,11 @@ export class MapComponent implements OnInit, OnDestroy {
       copyright: 'Contains OS data © Crown copyright and database right 2023',
     });
 
-    const map = new Map({
-      basemap: {
-        baseLayers: [wmtsLayer],
-      },
+    // Option 1: Add as a basemap
+    const customBasemap = new Basemap({
+      baseLayers: [wmtsLayer],
+      title: 'OS Maps',
     });
-
-    this.view = new MapView({
-      container: this.mapViewDiv?.nativeElement,
-      map,
-      center: [-4, 57],
-      constraints: { minScale: 2000, maxScale: 130000000 },
-      scale: 1000000,
-    });
-  }
-
-  /**
-   * The function initializes and adds Search, ScaleBar, and Compass widgets to the view in a TypeScript
-   * application.
-   */
-  private initialiazeWidgets(): void {
-    // Create the Search widget
-    const searchWidget = new Search({
-      view: this.view,
-    });
-    this.view.ui.add(searchWidget, {
-      position: 'top-right',
-    });
-
-    // Create the ScaleBar widget
-    const scaleBar = new ScaleBar({
-      view: this.view,
-      unit: 'dual',
-    });
-    this.view.ui.add(scaleBar, 'bottom-left');
-
-    // Create the Compass widget
-    const compass = new Compass({
-      view: this.view,
-    });
-    this.view.ui.add(compass, 'top-left');
+    this.mapViewDiv.nativeElement.basemap = customBasemap;
   }
 }
